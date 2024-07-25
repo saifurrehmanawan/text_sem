6d34013a64d414ac645861d513efad4665f0c853
saifurrehmanawan <145745643+saifurrehmanawan@users.noreply.github.com>
2024-07-25 14:31:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.info("This app compares two texts and provides a semantic similarity score ba
 text1 = st.text_area("Enter Text 1:")
 text2 = st.text_area("Enter Text 2:")
 
-if st.botton("Compare Texts"):
+if st.button("Compare Texts"):
   if text1 and text2:
     st.text("done")
   else:
