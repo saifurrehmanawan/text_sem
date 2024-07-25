0adee9abebfd83e49c011c44bbce151577a99102
saifurrehmanawan <145745643+saifurrehmanawan@users.noreply.github.com>
2024-07-25 14:27:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title('Text Similarity Check')
-st.text("This app compares two texts and provides a semantic similarity score based on their main content.")
+st.info("This app compares two texts and provides a semantic similarity score based on their main content.")
