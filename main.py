4fe4cda5f9e1e95b5abba9a47bbd42e98b73cf77
saifurrehmanawan <145745643+saifurrehmanawan@users.noreply.github.com>
2024-07-25 14:30:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.title('Text Similarity Check')
 st.info("This app compares two texts and provides a semantic similarity score based on their main content.")
+
+text1 = st.text_area("Enter Text 1:")
+text2 = st.text_area("Enter Text 2:")
+
+if st.botton("Compare Texts"):
+  if text1 and text2:
+    st.text("done")
+  else:
+    st.write("Please enter both texts to compare.")
